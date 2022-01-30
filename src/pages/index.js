ec41c0e2247295dd229f29725c98ceb66c70e1df
Gunn Hwang <gunhwang99@gmail.com>
2022-01-29 21:32:51 -0800

    Add favicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Helmet } from "react-helmet";
 import Navbar from "../components/Navbar";
 import HeroLayout from "../components/HeroLayout";
 import AboutLayout from "../components/AboutLayout";
@@ -26,6 +27,17 @@ const IndexPage = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Gunn Hwang Portfolio</title>
+        <link rel='apple-touch-icon' sizes='180x180' href='favicon/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='favicon/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='favicon/favicon-16x16.png' />
+        <link rel='manifest' href='favicon/site.webmanifest' />
+        <link rel='mask-icon' href='favicon/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#010b14' />
+        <meta name='theme-color' content='#010b14'></meta>
+      </Helmet>
       <Watermark>
         <h1>WORK IN PROGRESS</h1>
       </Watermark>
